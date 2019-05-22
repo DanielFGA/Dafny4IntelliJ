c1aec53004d013d0b803f172638dfffe9fc13a50
DanielFGA <daniel.figia@stud.fh-luebeck.de>
2019-05-23 00:49:25 +0200

    Exclude the parser. The syntax highlighting works without parser.

---
+++ src/test/java/DafnyLexerTest.java
@@ -0,0 +1,13 @@
+import com.intellij.lexer.Lexer;
+import com.intellij.testFramework.LexerTestCase;
+import com.intellij.testFramework.LightCodeInsightTestCase;
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
+
+public class DafnyLexerTest extends LightPlatformCodeInsightTestCase {
+
+
+
+
+}
